a915239d9420a97f7d2873ac7e03444d56f41283
dBerriff <berriff@icloud.com>
2024-01-26 20:51:02 +0000

    Update pwm_led.py

--- pwm_led.py
+++ pwm_led.py
@@ -8,35 +8,38 @@
 
 import asyncio
 from machine import Pin, PWM
+from random import randrange
 
 
-class PwmChannel:
+class PwmChannel(PWM):
     """
     Control PWM output
         - frequency and duty cycle: no range checking
-        - set_dc_u16() stores duty cycle; does not change state
+        - preset_dc_u16() stores the set duty cycle;
+          does not change state
         - call set_on() for output
     """
 
     def __init__(self, pwm_pin, frequency):
-        self.channel = PWM(Pin(pwm_pin), freq=frequency, duty_u16=0)
+        super().__init__(Pin(pwm_pin), freq=frequency, duty_u16=0)
+        self.pin = pwm_pin
         self._dc_u16 = 0
 
     def reset_freq(self, frequency):
         """ reset PWM frequency """
-        self.channel.freq(frequency)
+        self.freq(frequency)
 
-    def set_dc_u16(self, dc_u16):
+    def preset_dc_u16(self, dc_u16):
         """ set duty cycle by 16-bit unsigned integer """
         self._dc_u16 = dc_u16
 
     def set_on(self):
         """ set channel on """
-        self.channel.duty_u16(self._dc_u16)
+        self.duty_u16(self._dc_u16)
 
     def set_off(self):
         """ set channel off """
-        self.channel.duty_u16(0)
+        self.duty_u16(0)
  
  
 class Led(PwmChannel):
@@ -44,7 +47,7 @@ class Led(PwmChannel):
 
     @staticmethod
     def pc_u16(percentage):
-        """ convert positive percentage to 16-bit equivalent """
+        """ convert percentage to 16-bit equivalent """
         if 0 < percentage <= 100:
             return 0xffff * percentage // 100
         else:
@@ -55,21 +58,34 @@ class Led(PwmChannel):
         self.name = str(pwm_pin)
         # self.blink_lock = asyncio.Lock()
 
-    def set_dc_pc(self, percent):
+    def set_dc_u16(self, dc_u16_):
         """ set LED duty cycle """
-        self._dc_u16 = self.pc_u16(percent)
+        self.duty_u16(dc_u16_)
+        self._dc_u16 = dc_u16_
 
-    async def flash(self, ms_=500):
-        """ coro: flash the LED """
-        self.set_on()
-        await asyncio.sleep_ms(ms_)
-        self.set_off()
-        await asyncio.sleep_ms(ms_)
+    async def flash_u16(self, flash_u16_, duration_ms=500):
+        """ coro: flash the LED then restore dc """
+        self.duty_u16(flash_u16_)
+        await asyncio.sleep_ms(duration_ms)
+        self.duty_u16(self._dc_u16)
 
     async def blink(self, n):
-        """ coro: blink the LED n times """
+        """ coro: blink the LED n times at set dc """
         for _ in range(n):
             self.set_on()
             await asyncio.sleep_ms(100)
             self.set_off()
             await asyncio.sleep_ms(900)
+
+	async def twinkle(self, steady_u16):
+        """ replicate MERG gas-lamp twinkle
+        	- add/subract random values
+        """
+        while True:
+            # sum(steady, twinkle(5 - 10s), pop(every 10s))
+            pause = 5_000
+            twinkle = steady_u16 // 3
+            pop = steady_u16 // 2
+            sum_u16 = steady_u16 + twinkle - pop
+            self.duty_u16(sum_u16)
+            await asyncio.sleep_ms(pause)
