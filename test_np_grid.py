e897a91090bd36a8313e0bf5f3b60ca183d90890
dBerriff <berriff@icloud.com>
2024-02-08 09:38:48 +0000

    Update test_np_grid.py

--- test_np_grid.py
+++ test_np_grid.py
@@ -18,7 +18,7 @@ async def fill_grid(grid, rgb):
 
 
 async def traverse_strip(grid, rgb, pause_ms=20):
-    """ step through grid as a strip; cycling colour """
+    """ fill each pixel in strip order """
     for index in range(grid.n_pixels):
         grid[index] = rgb
         grid.write()
@@ -35,7 +35,7 @@ async def traverse_grid(grid, rgb, pause_ms=20):
 
 
 async def fill_cols(grid, rgb_set, pause_ms=20):
-    """ step through cols """
+    """ fill cols in order, cycling colours """
     n_colours = len(rgb_set)
     for c in range(grid.n_cols):
         grid.fill_col(c, rgb_set[c % n_colours])
@@ -44,7 +44,7 @@ async def fill_cols(grid, rgb_set, pause_ms=20):
 
 
 async def fill_rows(grid, rgb_set, pause_ms=20):
-    """ step through rows """
+    """ fill rows in order, cycling colours """
     n_colours = len(rgb_set)
     for r in range(grid.n_rows):
         grid.fill_row(r, rgb_set[r % n_colours])
@@ -53,7 +53,7 @@ async def fill_rows(grid, rgb_set, pause_ms=20):
 
 
 async def display_string(npg_, str_, rgb_, pause_ms=500):
-    """ cycle through the letters in a string """
+    """ dislay the letters in a string """
     for char_ in str_:
         npg_.display_char(char_, rgb_)
         npg_.write()
@@ -61,11 +61,11 @@ async def display_string(npg_, str_, rgb_, pause_ms=500):
 
 
 async def main():
-    """ set NeoPixel values on grid """
+    """ test setting NeoPixel values on grid """
 
     pin_number = 27
     npg = PixelGrid(pin_number, 8, 8)
-    npg.charset = charset  # load charset into object
+    npg.charset = charset  # load charset into npg; required
     off = (0, 0, 0)
     cs = ColourSpace()    
     level = 63
@@ -77,48 +77,48 @@ async def main():
     await fill_grid(npg, off)
 
     print('fill pixels as strip')
-    await traverse_strip(npg, rgb, 20)
+    await traverse_strip(npg, rgb)
     await asyncio.sleep_ms(1000)
-    await traverse_strip(npg, off, 20) 
+    await traverse_strip(npg, off) 
     await asyncio.sleep_ms(1000)
 
     print('fill pixels in col, row order')
-    await traverse_grid(npg, rgb, 20)
+    await traverse_grid(npg, rgb)
     await asyncio.sleep_ms(1000)
-    await traverse_grid(npg, off, 20) 
+    await traverse_grid(npg, off) 
     await asyncio.sleep_ms(1000)
      
-    # build list of rgb values
+    # build list of rgb values at same level
     rgb_set = cs.get_rgb_list(
         ('red', 'orange', 'yellow', 'green', 'blue', 'purple'), level)
 
     print('fill cols in sequence')
     await fill_cols(npg, rgb_set)
     await asyncio.sleep_ms(1000)
-    await fill_cols(npg, (off,))
+    await fill_cols(npg, (off,))  # list/tuple required
     await asyncio.sleep_ms(1000)
 
     print('fill rows in sequence')
     await fill_rows(npg, rgb_set)
     await asyncio.sleep_ms(1000)
-    await fill_rows(npg, (off,))
+    await fill_rows(npg, (off,))  # list/tuple required
     await asyncio.sleep_ms(1000)
 
     print('fill diagonals')
     rgb = cs.get_rgb('aqua', level)
-    pause = 1000
+    pause_ms = 1000
     for _ in range(12):
         npg.fill_diagonal(rgb)
         npg.write()
-        await asyncio.sleep_ms(pause)
+        await asyncio.sleep_ms(pause_ms)
         npg.fill_diagonal(off)
         npg.write()
         npg.fill_diagonal(rgb, mirror=True)
         npg.write()
-        await asyncio.sleep_ms(pause)
+        await asyncio.sleep_ms(pause_ms)
         npg.fill_diagonal(off, mirror=True)
         npg.write()
-        pause = pause // 2
+        pause_ms //= 2
     npg.clear()
     await asyncio.sleep_ms(2000)
 
@@ -131,7 +131,7 @@ async def main():
     await asyncio.sleep_ms(1000)
     await display_string(npg, '0123456789', rgb)
     npg.clear()
-    await asyncio.sleep_ms(1000)
+    await asyncio.sleep_ms(200)
     
 
 if __name__ == '__main__':
