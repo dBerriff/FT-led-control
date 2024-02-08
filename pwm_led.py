ff0a6f55379d4fc4d8c881b58ac27cb598509697
dBerriff <berriff@icloud.com>
2024-02-08 15:54:08 +0000

    Improve LED PWM helper funcions.

--- pwm_led.py
+++ pwm_led.py
@@ -11,8 +11,9 @@ import asyncio
 from machine import Pin, PWM
 from random import randrange
 
+
 # !!! rename
-class PwmChannel(PWM):
+class LedChannel(PWM):
     """
     Control PWM output
         - frequency and duty cycle: no range checking
@@ -21,34 +22,33 @@ class PwmChannel(PWM):
         - call set_on() for output
     """
 
-    def __init__(self, pwm_pin, frequency=800d):
+    # inheritance: MicroPython does not support setting
+    # freq and duty_u16 by super() keyword arguments
+    # frequency: 800Hz and 400Hz are most common for NeoPixels
+    def __init__(self, pwm_pin, frequency=800):
         super().__init__(Pin(pwm_pin))
         self.freq(frequency)
         self.duty_u16(0)
         self.pin = pwm_pin  # for debug
-        self._dc_u16 = 0
-        self._dc_u8 = 0
+        self.dc_u16 = 0  # for fade and other algorithms
 
     def reset_freq(self, frequency):
         """ reset PWM frequency """
         self.freq(frequency)
 
-    @property
-    def dc_u16(self):
-        return self._dc_u16
-
-    @dc_u16.setter
-    def dc_u16(self, dc_):
-        self._dc_u16 = dc_
-
     def set_dc_u16(self, dc_u16_):
-        """ set PWM duty cycle """
+        """ set PWM duty cycle and store value """
         self.duty_u16(dc_u16_)
         self.dc_u16 = dc_u16_
 
+    def set_off(self):
+        """ set 0 PWM duty cycle and store value """
+        self.duty_u16(0)
+        self.dc_u16 = 0
+
     def turn_on(self):
-        """ turn channel on at set duty cycle """
-        self.duty_u16(self._dc_u16)
+        """ turn channel on at stored duty cycle """
+        self.duty_u16(self.dc_u16)
 
     def turn_off(self):
         """ set channel off """
@@ -56,43 +56,12 @@ class PwmChannel(PWM):
  
     @staticmethod
     def u8_u16(dc_u8_):
-        """ convert 8-bit to proportional 16-bit level for PWM """
+        """ convert 8-bit to proportional 16-bit level """
         dc_u8_ = max(dc_u8_, 0)
         dc_u8_ = min(dc_u8_, 255)
         return (dc_u8_ << 8) + dc_u8_
 
     def set_dc_u8(self, dc_u8_):
         """ set LED u16 duty cycle from u8 """
-        self.dc_u8 = dc_u8_
-        dc_gc = self.dc_gamma[dc_u8_]
-        self.set_dc_u16(self.u8_u16(dc_gc))
-
-    async def blink(self, dc_u8, n):
-        """ coro: blink the LED n times at set dc """
-        self.dc_u8 = dc_u8
-        dc_u16 = self.u8_u16(self.dc_gamma[dc_u8])
-        for _ in range(n):
-            self.set_dc_u16(dc_u16)
-            await asyncio.sleep_ms(100)
-            self.set_dc_u16(0x0000)
-            await asyncio.sleep_ms(900)
-
-    async def fade_in(self, dc_u8, period=1000):
-        """ fade-in to set dc """
-        step_pause = period // dc_u8
-        dc = 0
-        while dc < dc_u8:
-            self.set_dc_u8(dc)
-            dc += 1
-            await asyncio.sleep_ms(step_pause)
-        self.set_dc_u8(dc_u8)
-
-    async def fade_out(self, period=1000):
-        """ fade-out to set dc% """
-        step_pause = period // self.dc_u8
-        dc = self.dc_u8
-        while dc > 0:
-            self.set_dc_u8(dc)
-            dc -= 1
-            await asyncio.sleep_ms(step_pause)
-        self.set_dc_u8(0)
+        self.dc_u16 = self.u8_u16(dc_u8_)
+        self.duty_u16(self.duty_u16)
--- test_led.py
+++ test_led.py
@@ -3,42 +3,104 @@
 """ test LED- and NeoPixel-related classes """
 
 import asyncio
-from pwm_led import Led
-from colour_space import get_rgb_gamma
+from pwm_led import LedChannel
+from colour_space import ColourSpace
 
-async def twin_flash(led_1, led_2, level_, period_ms):
-    """ flash alternating leds every period """
+# helper coroutines
+
+async def blink(led_, dc_u8, n):
+    """ coro: blink the LED n times at dc_u8 """
+    dc = led_.u8_u16(dc_u8)
+    led_.dc_u16 = dc
+    for _ in range(n):
+        led_.duty_u16(dc)
+        await asyncio.sleep_ms(100)
+        led_.duty_u16(0)
+        await asyncio.sleep_ms(900)
+
+async def fade_in(led_, dc_gamma_, lin_level_, period=1000):
+    """ coro: fade-in from 0 to lin_level
+        - lin_level is uncompensated linear u8 level
+        - gamma compensation is applied to each setting
+    """
+    if lin_level_ == 0:
+        return
+    step_pause = period // lin_level_
+    fade_dc = 0
+    while fade_dc < lin_level_:
+        led_.duty_u16(led_.u8_u16(dc_gamma_[fade_dc]))
+        fade_dc += 1
+        await asyncio.sleep_ms(step_pause)
+    led_.dc_u16 = led_.u8_u16(dc_gamma_[lin_level_])
+    led_.duty_u16(led_.dc_u16)
+
+async def fade_out(led_, dc_gamma_, lin_level_, period=1000):
+    """ coro: fade-out from lin_level to 0
+        - lin_level is uncompensated linear u8 level
+        - gamma compensation is applied to each setting
+    """
+    if lin_level_ == 0:
+        return
+    step_pause = period // lin_level_
+    fade_dc = lin_level_
+    while fade_dc > 0:
+        led_.duty_u16(led_.u8_u16(dc_gamma_[fade_dc]))
+        fade_dc -= 1
+        await asyncio.sleep_ms(step_pause)
+    led_.dc_u16 = 0
+    led_.duty_u16(0)
+
+async def twin_flash(led_1, led_2, level_, period_ms=500):
+    """ coro: flash alternating leds every period """
     led_1.dc_u16 = led_1.u8_u16(level_)
     led_2.dc_u16 = led_1.dc_u16
 
-    for _ in range(20):
+    while True:
         led_1.turn_on()
         led_2.turn_off()
         await asyncio.sleep_ms(period_ms)
         led_1.turn_off()
         led_2.turn_on()
         await asyncio.sleep_ms(period_ms)
-    led_2.turn_off()
 
 
 async def main():
+    # build gamma-correction tuple
+    dc_gamma = ColourSpace().RGB_GAMMA
 
-    led_a = Led(pwm_pin=9)
+    led_a = LedChannel(pwm_pin=10)
     led_a.set_dc_u16(0)
-    led_b = Led(pwm_pin=10)
+    print(f'led_a: {led_a}')
+    led_b = LedChannel(pwm_pin=11)
     led_b.set_dc_u16(0)
+    print(f'led_b: {led_b}')
+    lin_level = 191
+    level = dc_gamma[lin_level]
+    print(f'linear level: {lin_level} gamma-compensated level: {level}')
 
     await asyncio.sleep_ms(200)
-    await led_a.blink(255, 5)
-    await asyncio.sleep_ms(1000)
+    print('blink')
+    asyncio.create_task(blink(led_a, level, 5))
+    asyncio.create_task(blink(led_b, level, 5))
+    await asyncio.sleep_ms(6_000)
     
-    await led_a.fade_in(255, 10_000)
+    print('fade in')
+    await fade_in(led_a, dc_gamma, lin_level)
     print('fade in complete')
-    await asyncio.sleep_ms(1000)
-    await led_a.fade_out(10_000)
+    await asyncio.sleep_ms(1_000)
+
+    print('fade out')
+    await fade_out(led_a, dc_gamma, lin_level)
     print('fade out complete')
-    await asyncio.sleep_ms(1000)
-    await twin_flash(led_a, led_b, 127, 500)
+    await asyncio.sleep_ms(1_000)
+
+    print('twin flash')
+    tf_task = asyncio.create_task(twin_flash(led_a, led_b, level))
+    await asyncio.sleep_ms(5_000)
+    tf_task.cancel()
+    led_a.turn_off()
+    led_b.turn_off()
+    await asyncio.sleep_ms(1_000)
 
 
 if __name__ == '__main__':
